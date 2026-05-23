748dd3b131e8f0620a6bccc1c1b42f3b177e9551
unknown <miguelreiriz14@gmail.com>
2026-05-23 16:37:04 +0200

    feat: add UserRequestDTO and UserResponseDTO

---
+++ src/main/java/com/miguel/taskmanager/dto/UserRequestDTO.java
@@ -0,0 +1,9 @@
+package com.miguel.taskmanager.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequestDTO {
+    String username;
+    String password;
+}
---
+++ src/main/java/com/miguel/taskmanager/dto/UserResponseDTO.java
@@ -0,0 +1,9 @@
+package com.miguel.taskmanager.dto;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+    Long userID;
+    String username;
+}
