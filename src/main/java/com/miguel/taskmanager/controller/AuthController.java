ffceec25cd27a509f61580423a60a0e3d93c7ef3
unknown <miguelreiriz14@gmail.com>
2026-05-24 13:51:42 +0200

    feat: add register and login endpoints to AuthController

--- src/main/java/com/miguel/taskmanager/controller/AuthController.java
+++ src/main/java/com/miguel/taskmanager/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.miguel.taskmanager.controller;
 
+import com.miguel.taskmanager.dto.UserRequestDTO;
 import com.miguel.taskmanager.entity.User;
+import com.miguel.taskmanager.service.JwtService;
+import com.miguel.taskmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    public AuthController(UserService userService, JwtService jwtService) {
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserRequestDTO userRequestDTO){
+        String user = userService.createUser(userRequestDTO).getUsername();
+        return jwtService.generateToken(user);
+    }
+
     @PostMapping("/login")
-    public String login(@RequestBody User user){
-        return "TOKEN_PRUEBA";
+    public String login(@RequestBody UserRequestDTO userRequestDTO){
+        return userService.login(userRequestDTO);
     }
 }
